e3c6cc52da13388423c459a4bf6117ba9a44c5f0
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-01-28 13:40:30 +0500

    Added

--- apps/dashboard/src/app/[locale]/(app)/chatassistant/page.tsx
+++ apps/dashboard/src/app/[locale]/(app)/chatassistant/page.tsx
@@ -2,7 +2,7 @@
 import { Metadata } from 'next';
 import dynamic from 'next/dynamic';
 import { BaseChat } from '@/components/chat-assistant/chat/BaseChat';
-// import { Header } from '@/components/chat-assistant/header/Header';
+import { Header } from '@/components/chat-assistant/header/Header';
 const Chat = dynamic(() => import('@/components/chat-assistant/chat/Chat.client').then((mod) => mod.Chat), {
   ssr: false, // Disable server-side rendering
   loading: () => <BaseChat />, // Fallback component
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 export default function IndexPage() {
   return (
     <div className="flex flex-col h-full w-full bg-bolt-elements-background-depth-1">
-      {/* <Header  /> */}
+      <Header  />
       <Chat />
     </div>
   );
