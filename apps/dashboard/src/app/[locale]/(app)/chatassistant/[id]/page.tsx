373d8d78d8b2159b0b753950fa64880a292757d1
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-01-28 11:22:05 +0500

    Added

---
+++ apps/dashboard/src/app/[locale]/(app)/chatassistant/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import IndexRoute from '../page'
+
+type ChatIdPageProps = {
+  params: { id: string };
+};
+
+export default function ChatIdPage({ params }: ChatIdPageProps) {
+  return (
+    <div>
+      {/* <IndexRoute /> */}
+    </div>
+  );
+}
