218e3363bce08713b3b6e1454ff44cd2abfe65e4
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-02-03 02:22:06 +0500

    Added

--- apps/dashboard/src/lib/persistence/db.ts
+++ apps/dashboard/src/lib/persistence/db.ts
@@ -66,7 +66,7 @@ export async function setMessages(
     const request = store.put({
       id,
       messages,
-      urlId: urlId || id,
+      urlId,
       description,
       timestamp: timestamp ?? new Date().toISOString(),
     });
