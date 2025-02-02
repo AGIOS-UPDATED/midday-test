1a915a1cb5800b0d417afce4b1421772afbafa5b
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-02-02 20:52:46 +0500

    Update page.tsx

--- apps/dashboard/src/app/[locale]/(app)/chatassistant/[id]/page.tsx
+++ apps/dashboard/src/app/[locale]/(app)/chatassistant/[id]/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 import React from 'react';
-import IndexRoute from '../page'
 import dynamic from 'next/dynamic';
 import  'react-toastify/dist/ReactToastify.css';
 import '../../../../../styles/chat-assistant-styles/index.scss';
+import { Header } from '@/components/header';
 
 const ChatComponent = dynamic(
   () => import('@/components/chat-assistant/chat/Chat.client'),
@@ -19,8 +19,11 @@ type ChatIdPageProps = {
 
 export default function ChatIdPage({ params }: ChatIdPageProps) {
   return (
-    <div>
-         <ChatComponent />
-    </div>
+    <div className="flex flex-col h-full  w-full bg-bolt-elements-background-depth-1">
+    <Header/>
+
+    <ChatComponent />
+
+  </div>
   );
 }
