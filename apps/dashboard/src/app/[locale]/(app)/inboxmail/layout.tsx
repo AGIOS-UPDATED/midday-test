35cc3a3406f490ba406b1cda9846bc65dfe35c19
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-02-15 03:40:12 +0500

    Update layout.tsx

--- apps/dashboard/src/app/[locale]/(app)/inboxmail/layout.tsx
+++ apps/dashboard/src/app/[locale]/(app)/inboxmail/layout.tsx
@@ -1,10 +1,12 @@
 import { AppSidebar } from "@/components/inbox/ui/app-sidebar";
-
+import { SidebarProvider } from "@/components/inbox/ui/sidebar";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
+    <SidebarProvider>
     <div className="flex h-screen w-screen overflow-hidden">
-      {/* <AppSidebar /> */}
+      <AppSidebar />
       {children}
     </div>
+    </SidebarProvider>
   );
 }
