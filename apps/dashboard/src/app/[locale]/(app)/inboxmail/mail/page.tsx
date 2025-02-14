d7346fa894cb8c3a8eebc5732737cf031926da18
Syed Sameer Shah <78991984+sscodez@users.noreply.github.com>
2025-02-15 04:49:36 +0500

    Added

--- apps/dashboard/src/app/[locale]/(app)/inboxmail/mail/page.tsx
+++ apps/dashboard/src/app/[locale]/(app)/inboxmail/mail/page.tsx
@@ -14,13 +14,13 @@ export default async function MailPage() {
   return (
     <div className="w-full bg-white dark:bg-sidebar">
       <div className="flex-col dark:bg-[#090909] dark:text-gray-100 md:m-2 md:flex md:rounded-md md:border">
-        {/* <Mail
+        <Mail
           accounts={accounts}
           mails={mails}
           defaultLayout={defaultLayout}
           defaultCollapsed={defaultCollapsed}
           navCollapsedSize={4}
-        /> */}
+        />
       </div>
     </div>
   );
